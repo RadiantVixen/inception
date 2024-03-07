fbb7972c780da965ff7a61aa7cc4ae862f9ab61d
aatki <aatki@aatki.42.fr>
2024-03-07 08:31:41 +0100

    push

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 all :
 	docker compose -f srcs/docker-compose.yml up --build -d
 clean :
+	sudo rm -rf /home/aatki/data/wordpress/*
 	docker compose -f srcs/docker-compose.yml down -v
 fclean : clean
 	docker compose -f srcs/docker-compose.yml
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-wp config create --dbname=$mariaDB_DATABASE --dbuser=$mariaDB_USER --dbpass=$mariaDB_PASSWORD --allow-root --dbhost=mariadb
+sleep 2
+cd /var/www/wordpress
+wp config create --dbname=mariadb --dbuser=mariadb_user --dbpass=mariadb_u_p --allow-root --dbhost=mariadb
+sleep 2
 wp core install --url=aatki.42.fr --allow-root  --title=aicha_website  --admin_user=aicha  --admin_password=aatki  --admin_email=aicha@gmail.com
-
+wp user create 
 /usr/sbin/php-fpm7.4 -F
-
