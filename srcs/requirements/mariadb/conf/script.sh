24dee1da648fde3dfe8c22ad564c51ebe370e7e5
aicha7773 <aichaatki067@gmail.com>
2024-03-09 09:49:20 +0100

    push

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -2,12 +2,12 @@
 
 service mariadb start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${mariaDB_DATABASE}\`;"  \
-&& mysql -e "CREATE USER IF NOT EXISTS \`${mariaDB_USER}\`@'%' IDENTIFIED BY '${mariaDB_PASSWORD}';" \
-&& mysql -e "GRANT ALL PRIVILEGES ON \`${mariaDB_DATABASE}\`.* TO \`${mariaDB_USER}\`@'%' IDENTIFIED BY '${mariaDB_PASSWORD}';" \
-&& mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${mariaDB_ROOT_PASSWORD}';" \
-&& mysql -e "FLUSH PRIVILEGES;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${mariaDB_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${mariaDB_USER}\`@'%' IDENTIFIED BY '${mariaDB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${mariaDB_DATABASE}\`.* TO \`${mariaDB_USER}\`@'%' IDENTIFIED BY '${mariaDB_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'%' IDENTIFIED BY '${mariaDB_ROOT_PASSWORD}';" \
+mariadb -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$mariaDB_ROOT_PASSWORD shutdown
+service mariadb stop;
 
 mysqld_safe
